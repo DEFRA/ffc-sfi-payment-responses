9e21ef1f5a0191c30cc201218391351cb0c7c505
John Watson <johnwatson484@hotmail.com>
2021-08-28 13:53:02 +0100

    Config setup

---
+++ app/config/index.js
@@ -0,0 +1,36 @@
+const Joi = require('joi')
+const mqConfig = require('./mq-config')
+const storageConfig = require('./storage-config')
+
+// Define config schema
+const schema = Joi.object({
+  env: Joi.string().valid('development', 'test', 'production').default('development')
+})
+
+// Build config
+const config = {
+  env: process.env.NODE_ENV
+}
+
+// Validate config
+const result = schema.validate(config, {
+  abortEarly: false
+})
+
+// Throw if config is invalid
+if (result.error) {
+  throw new Error(`The server config is invalid. ${result.error.message}`)
+}
+
+// Use the Joi validated value
+const value = result.value
+
+// Add some helper props
+value.isDev = value.env === 'development'
+value.isTest = value.env === 'test'
+value.isProd = value.env === 'production'
+value.acknowledgementTopic = mqConfig.acknowledgementTopic
+value.returnTopic = mqConfig.returnTopic
+value.storageConfig = storageConfig
+
+module.exports = value
---
+++ app/config/mq-config.js
@@ -0,0 +1,53 @@
+const joi = require('joi')
+
+const mqSchema = joi.object({
+  messageQueue: {
+    host: joi.string(),
+    username: joi.string(),
+    password: joi.string(),
+    useCredentialChain: joi.bool().default(false),
+    appInsights: joi.object()
+  },
+  acknowledgementTopic: {
+    name: joi.string(),
+    address: joi.string()
+  },
+  returnTopic: {
+    name: joi.string(),
+    address: joi.string()
+  }
+})
+const mqConfig = {
+  messageQueue: {
+    host: process.env.MESSAGE_QUEUE_HOST,
+    username: process.env.MESSAGE_QUEUE_USER,
+    password: process.env.MESSAGE_QUEUE_PASSWORD,
+    useCredentialChain: process.env.NODE_ENV === 'production',
+    appInsights: process.env.NODE_ENV === 'production' ? require('applicationinsights') : undefined
+  },
+  acknowledgementTopic: {
+    name: process.env.ACKNOWLEDGEMENT_TOPIC_NAME,
+    address: process.env.ACKNOWLEDGEMENT_TOPIC_ADDRESS
+  },
+  returnTopic: {
+    name: process.env.RETURN_TOPIC_NAME,
+    address: process.env.RETURN_TOPIC_ADDRESS
+  }
+}
+
+const mqResult = mqSchema.validate(mqConfig, {
+  abortEarly: false
+})
+
+// Throw if config is invalid
+if (mqResult.error) {
+  throw new Error(`The message queue config is invalid. ${mqResult.error.message}`)
+}
+
+const acknowledgementTopic = { ...mqResult.value.messageQueue, ...mqResult.value.acknowledgementTopic }
+const returnTopic = { ...mqResult.value.messageQueue, ...mqResult.value.returnTopic }
+
+module.exports = {
+  acknowledgementTopic,
+  returnTopic
+}
