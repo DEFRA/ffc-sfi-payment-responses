b3baf5edea3bcc21b8338fe9d57509a3874ae09e
John Watson <johnwatson484@hotmail.com>
2021-08-30 19:35:25 +0100

    Return file parsing

--- app/processing/parse-return-file.js
+++ app/processing/parse-return-file.js
@@ -1,5 +1,22 @@
+const { convertToPence } = require('../currency-convert')
+
 const parseReturnFile = async (buffer) => {
-  return []
+  const csv = buffer.toString().trim().split('\r\n')
+  return csv.map(x => {
+    const row = x.split(',')
+    return {
+      sourceSystem: row[0],
+      invoiceNumber: row[1],
+      frn: Number(row[2]),
+      postedDate: row[4],
+      currency: row[5] === 'S' ? 'GBP' : row[5],
+      value: convertToPence(row[6]),
+      settlementDate: row[7],
+      reference: row[8],
+      settled: row[9] === 'D',
+      detail: row[10]
+    }
+  })
 }
 
 module.exports = parseReturnFile
--- app/processing/process-return.js
+++ app/processing/process-return.js
@@ -7,7 +7,9 @@ const processReturn = async (filename) => {
   const buffer = await blobStorage.downloadFile(filename)
   try {
     const messages = await parseReturnFile(buffer)
-    await sendReturnMessages(messages)
+    if (messages.length) {
+      await sendReturnMessages(messages)
+    }
     await blobStorage.archiveFile(filename, filename)
   } catch (err) {
     console.error(`Quarantining ${filename}, failed to parse file`, err)
