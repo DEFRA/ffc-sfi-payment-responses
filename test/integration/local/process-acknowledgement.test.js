7a6756391bbbd9641066a087cc33ca40f21fe093
John Watson <johnwatson484@hotmail.com>
2021-09-02 21:20:55 +0100

    Add tests

--- test/integration/local/process-acknowledgement.test.js
+++ test/integration/local/process-acknowledgement.test.js
@@ -15,16 +15,25 @@ const { BlobServiceClient } = require('@azure/storage-blob')
 const config = require('../../../app/config')
 const path = require('path')
 let blobServiceClient
-let containerClient
+let inboundContainer
+let archiveContainer
+let quarantineContainer
 const TEST_FILE = path.resolve(__dirname, '../../files/acknowledgement.xml')
+const TEST_INVALID_FILE = path.resolve(__dirname, '../../files/broken-acknowledgement.xml')
 
 describe('process acknowledgement', () => {
   beforeAll(async () => {
     blobServiceClient = BlobServiceClient.fromConnectionString(config.storageConfig.connectionStr)
-    containerClient = blobServiceClient.getContainerClient(config.storageConfig.inboundContainer)
-    await containerClient.deleteIfExists()
-    await containerClient.createIfNotExists()
-    const blockBlobClient = containerClient.getBlockBlobClient('mock_0001_Ack.xml')
+    inboundContainer = blobServiceClient.getContainerClient(config.storageConfig.inboundContainer)
+    archiveContainer = blobServiceClient.getContainerClient(config.storageConfig.archiveContainer)
+    quarantineContainer = blobServiceClient.getContainerClient(config.storageConfig.quarantineContainer)
+    await inboundContainer.deleteIfExists()
+    await archiveContainer.deleteIfExists()
+    await quarantineContainer.deleteIfExists()
+    await inboundContainer.createIfNotExists()
+    await archiveContainer.createIfNotExists()
+    await quarantineContainer.createIfNotExists()
+    const blockBlobClient = inboundContainer.getBlockBlobClient('mock_0001_Ack.xml')
     await blockBlobClient.uploadFile(TEST_FILE)
   })
 
@@ -46,5 +55,36 @@ describe('process acknowledgement', () => {
   test('sends success', async () => {
     await processing.start()
     expect(mockSendBatchMessages.mock.calls[0][0][0].body.success).toBe(true)
+  })
+
+  test('archives file on success', async () => {
+    await processing.start()
+    const fileList = []
+    for await (const item of archiveContainer.listBlobsFlat()) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === 'mock_0001_Ack.xml').length).toBe(1)
+  })
+
+  test('ignores unrelated file', async () => {
+    const blockBlobClient = inboundContainer.getBlockBlobClient('ignore me.xml')
+    await blockBlobClient.uploadFile(TEST_FILE)
+    await processing.start()
+    const fileList = []
+    for await (const item of inboundContainer.listBlobsFlat()) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === 'ignore me.xml').length).toBe(1)
+  })
+
+  test('quarantines invalid file', async () => {
+    const blockBlobClient = inboundContainer.getBlockBlobClient('mock_0001_Ack.xml')
+    await blockBlobClient.uploadFile(TEST_INVALID_FILE)
+    await processing.start()
+    const fileList = []
+    for await (const item of quarantineContainer.listBlobsFlat()) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === 'mock_0001_Ack.xml').length).toBe(1)
   })
 })
