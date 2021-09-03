9ddb4a139eb45a11f3e4034ab07d658621648162
John Watson <johnwatson484@hotmail.com>
2021-09-03 17:32:06 +0100

    Simplify ack mapping

--- app/processing/parse-acknowledgement-file.js
+++ app/processing/parse-acknowledgement-file.js
@@ -3,14 +3,12 @@ const xml2js = require('xml2js')
 const parseAcknowledgementFile = async (xml) => {
   const parser = new xml2js.Parser()
   const acknowledgementData = await parser.parseStringPromise(xml)
-  return acknowledgementData.Envelope.Lines[0].Line.map(x => {
-    return {
-      invoiceNumber: x.Invoice[0],
-      frn: Number(x.SupplierID[0]),
-      success: x.Success[0].toLowerCase() === 'true',
-      acknowledged: new Date()
-    }
-  })
+  return acknowledgementData.Envelope.Lines[0].Line.map(x => ({
+    invoiceNumber: x.Invoice[0],
+    frn: Number(x.SupplierID[0]),
+    success: x.Success[0].toLowerCase() === 'true',
+    acknowledged: new Date()
+  }))
 }
 
 module.exports = parseAcknowledgementFile
