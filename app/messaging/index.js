c4904d566e17bb2a1c8387b3dababfbd5242faad
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-01-14 10:23:54 +0000

    Remove references to SFI

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -2,11 +2,11 @@ const sendBatchMessage = require('./send-batch-message')
 const config = require('../config/mq-config')
 
 const sendAcknowledgementMessages = async (payload) => {
-  await sendBatchMessage(payload, 'uk.gov.sfi.payment.acknowledgement', config.acknowledgementTopic)
+  await sendBatchMessage(payload, 'uk.gov.pay.acknowledgement', config.acknowledgementTopic)
 }
 
 const sendReturnMessages = async (payload) => {
-  await sendBatchMessage(payload, 'uk.gov.sfi.payment.return', config.returnTopic)
+  await sendBatchMessage(payload, 'uk.gov.pay.return', config.returnTopic)
 }
 
 module.exports = {
