8ddb31933801d75e6e0356b36872d37347160428
Simon Dunn <simondunn90@gmail.com>
2023-01-12 14:26:53 +0000

    tidy up test file

--- test/integration/local/process-payment-file.test.js
+++ test/integration/local/process-payment-file.test.js
@@ -1,4 +1,13 @@
+const path = require('path')
+const processing = require('../../../app/processing')
+const { BlobServiceClient } = require('@azure/storage-blob')
+const config = require('../../../app/config')
+const TEST_FILE = path.resolve(__dirname, '../../files/payment-file.csv')
+const RETURN_TEST_FILE = path.resolve(__dirname, '../../files/return.csv')
+const ACK_TEST_FILE = path.resolve(__dirname, '../../files/acknowledgement.xml')
 const mockSendBatchMessages = jest.fn()
+let container
+let blobServiceClient
 jest.mock('ffc-messaging', () => {
   return {
     MessageBatchSender: jest.fn().mockImplementation(() => {
@@ -9,16 +18,6 @@ jest.mock('ffc-messaging', () => {
     })
   }
 })
-jest.useFakeTimers()
-const processing = require('../../../app/processing')
-const { BlobServiceClient } = require('@azure/storage-blob')
-const config = require('../../../app/config')
-const path = require('path')
-let blobServiceClient
-let container
-const TEST_FILE = path.resolve(__dirname, '../../files/payment-file.csv')
-const RETURN_TEST_FILE = path.resolve(__dirname, '../../files/return.csv')
-const ACK_TEST_FILE = path.resolve(__dirname, '../../files/acknowledgement.xml')
 
 describe('process payment files', () => {
   beforeAll(async () => {
@@ -30,7 +29,7 @@ describe('process payment files', () => {
     await blockBlobClient.uploadFile(TEST_FILE)
   })
 
-  test('removes all payment files', async () => {
+  test('Should remove all payment files contained within the blob storage container.', async () => {
     await processing.start()
 
     const inboundFileList = []
@@ -47,7 +46,7 @@ describe('process payment files', () => {
     expect(archiveFileList.filter(x => x === `${config.storageConfig.archiveFolder}/FFC mock Payment File.csv`).length).toBe(0)
   })
 
-  test('removes all payment files and return file is archived', async () => {
+  test('Should remove all payment files contained within the blob storage container and archive the return file.', async () => {
     const blockBlobClient = container.getBlockBlobClient(`${config.storageConfig.inboundFolder}/mock Return File.csv`)
     await blockBlobClient.uploadFile(RETURN_TEST_FILE)
     await processing.start()
@@ -67,7 +66,7 @@ describe('process payment files', () => {
     expect(archiveFileList.filter(x => x === `${config.storageConfig.archiveFolder}/mock Return File.csv`).length).toBe(1)
   })
 
-  test('removes all payment files and acknowledgement file is archived', async () => {
+  test('Should remove all payment files contained within the blob storage container and archive the ackowledgement file.', async () => {
     const blockBlobClient = container.getBlockBlobClient(`${config.storageConfig.inboundFolder}/mock_0001_Ack.xml`)
     await blockBlobClient.uploadFile(ACK_TEST_FILE)
     await processing.start()
