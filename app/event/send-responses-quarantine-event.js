737b633f4411e7cef4cc2b1f5bbc40e297120713
Simon Dunn <simondunn90@gmail.com>
2023-06-22 15:35:53 +0100

    158763-remove-v1-events (#29)

--- app/event/send-responses-quarantine-event.js
+++ app/event/send-responses-quarantine-event.js
@@ -1,30 +1,12 @@
-const { v4: uuidv4 } = require('uuid')
-const raiseEvent = require('./raise-event')
 const config = require('../config')
 const { EventPublisher } = require('ffc-pay-event-publisher')
 const { RESPONSE_REJECTED } = require('../constants/events')
 const { SOURCE } = require('../constants/source')
 
 const sendResponsesQuarantineEvent = async (filename, error) => {
-  if (config.useV1Events) {
-    await sendV1ResponsesQuarantineEvent(filename)
-  }
   if (config.useV2Events) {
     await sendV2ResponsesQuarantineEvent(filename, error)
   }
-}
-
-const sendV1ResponsesQuarantineEvent = async (filename) => {
-  const event = {
-    id: uuidv4(),
-    name: 'responses-processing-quarantine-error',
-    type: 'error',
-    message: `Quarantined ${filename}`,
-    data: {
-      filename
-    }
-  }
-  await raiseEvent(event, 'error')
 }
 
 const sendV2ResponsesQuarantineEvent = async (filename, error) => {
