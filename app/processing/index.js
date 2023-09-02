0a03d4cc4ad96cabb60803412f866f7db83e5118
John Watson <john.watson1@defra.gov.uk>
2023-09-02 20:10:48 +0100

    Add locking

--- app/processing/index.js
+++ app/processing/index.js
@@ -1,5 +1,6 @@
 const blobStorage = require('../storage')
 const config = require('../config')
+const db = require('../data')
 const isAcknowledgementFile = require('./is-acknowledgement-file')
 const processAcknowledgement = require('./process-acknowledgement')
 const isReturnFile = require('./is-return-file')
@@ -8,7 +9,10 @@ const isPaymentFile = require('./is-payment-file')
 const processPaymentFile = require('./process-payment-file')
 
 const start = async () => {
+  const transaction = await db.sequelize.transaction()
   try {
+    await db.lock.findByPk(1, { transaction, lock: true })
+
     const filenameList = await blobStorage.getInboundFileList()
 
     if (filenameList.length > 0) {
@@ -22,8 +26,10 @@ const start = async () => {
         }
       }
     }
+    await transaction.commit()
   } catch (err) {
     console.error(err)
+    await transaction.rollback()
   } finally {
     setTimeout(start, config.processingInterval)
   }
