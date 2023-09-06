3adc96865cc26ec7f5df02d536fd420f6b04bf59
John Watson <john.watson1@defra.gov.uk>
2023-09-06 15:01:33 +0100

    Update tests

--- test/unit/processing/returns/is-return-file.test.js
+++ test/unit/processing/returns/is-return-file.test.js
@@ -1,4 +1,4 @@
-const isReturnFile = require('../../../app/processing/returns/is-return-file')
+const { isReturnFile } = require('../../../../app/processing/returns/is-return-file')
 
 const paymentFiles = ['FFCSFIP_0001_AP_20220329120821 (SITISFI).csv', 'FFCSFIP_0001_AR_20220329120821 (SITISFI).csv']
 const returnFile = 'FFCSITI_SFI Return File.csv'
