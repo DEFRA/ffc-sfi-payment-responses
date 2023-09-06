a0b3bef1c8ba1d6be8718d1b1dff51b4ff1cc270
John Watson <john.watson1@defra.gov.uk>
2023-09-06 09:40:04 +0100

    Update tests

--- test/unit/processing/payments/process-payment-file.test.js
+++ test/unit/processing/payments/process-payment-file.test.js
@@ -1,12 +1,10 @@
-
-jest.mock('../../../app/storage')
-const blobStorage = require('../../../../app/storage')
+jest.mock('../../../../app/storage')
 const { deleteFile } = require('../../../../app/storage')
 
-const processPaymentFile = require('../../../../app/processing/payments/process-payment-file')
+const { processPaymentFile } = require('../../../../app/processing/payments/process-payment-file')
 
 const filename = 'FFCSFIP_0001_AP_20220329120821 (SITISFI).csv'
-const errorMessage = 'Error when calling blobStorage.delete'
+const errorMessage = 'Error when calling delete'
 jest.spyOn(console, 'log').mockImplementation()
 jest.spyOn(console, 'error').mockImplementation()
 
@@ -15,49 +13,49 @@ describe('process payment file', () => {
     jest.resetAllMocks()
   })
 
-  test('Should call blobStorage.deleteFile', async () => {
+  test('Should call deleteFile', async () => {
     await processPaymentFile(filename)
-    expect(blobStorage.deleteFile).toBeCalled()
+    expect(deleteFile).toBeCalled()
   })
 
-  test('Should call blobStorage.deleteFile once', async () => {
+  test('Should call deleteFile once', async () => {
     await processPaymentFile(filename)
-    expect(blobStorage.deleteFile).toBeCalledTimes(1)
+    expect(deleteFile).toBeCalledTimes(1)
   })
 
-  test('Should call blobStorage.deleteFile with filename', async () => {
+  test('Should call deleteFile with filename', async () => {
     await processPaymentFile(filename)
-    expect(blobStorage.deleteFile).toHaveBeenCalledWith(filename)
+    expect(deleteFile).toHaveBeenCalledWith(filename)
   })
 
-  test('Should log message when blobStorage.deleteFile is called', async () => {
+  test('Should log message when deleteFile is called', async () => {
     await processPaymentFile(filename)
     expect(console.log).toHaveBeenCalled()
   })
 
-  test('Should log one message when blobStorage.deleteFile is called', async () => {
+  test('Should log one message when deleteFile is called', async () => {
     await processPaymentFile(filename)
     expect(console.log).toHaveBeenCalledTimes(1)
   })
 
-  test('Should log message including filename when blobStorage.deleteFile is called', async () => {
+  test('Should log message including filename when deleteFile is called', async () => {
     await processPaymentFile(filename)
-    expect(console.log).toHaveBeenCalledWith('Payment File sent from DAX, has been deleted :', filename)
+    expect(console.log).toHaveBeenCalledWith('Payment file sent from DAX, has been deleted :', filename)
   })
 
-  test('Should call console.error when blobStorage.deleteFile throws an error', async () => {
+  test('Should call console.error when deleteFile throws an error', async () => {
     deleteFile.mockRejectedValue(new Error(errorMessage))
     await processPaymentFile(filename)
     expect(console.error).toBeCalled()
   })
 
-  test('Should call console.error once when blobStorage.deleteFile throws an error', async () => {
+  test('Should call console.error once when deleteFile throws an error', async () => {
     deleteFile.mockRejectedValue(new Error(errorMessage))
     await processPaymentFile(filename)
     expect(console.error).toBeCalledTimes(1)
   })
 
-  test('Should call console.error when blobStorage.deleteFile throws an error', async () => {
+  test('Should call console.error when deleteFile throws an error', async () => {
     deleteFile.mockRejectedValue(new Error(errorMessage))
     await processPaymentFile(filename)
     expect(console.error).toHaveBeenCalledWith(`Failed to delete payment file: ${filename}`, new Error(errorMessage))
